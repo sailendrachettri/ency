4456cd83c41036e191b6c5e3091ee041e9e8ab59
sailendrachettri <sailendra9083@gmail.com>
2023-06-12 23:34:20 +0530

    footer refactored

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ export default function App() {
     return (
         <>
             <footer className="font-small blue mt-3" id='footer'>
-                <div className="text-center py-4">© {year} Copyright |
+                <div className="text-center py-4">  Copyright © {year}
                     <Link to="/about" style={{ textDecoration: "none" }}> Sailendra Chettri</Link>
                 </div>
             </footer>

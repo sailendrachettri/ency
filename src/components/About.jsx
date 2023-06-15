fd632e53b140ff3e011c5e7561c988dc884ea8d8
sailendrachettri <sailendra9083@gmail.com>
2023-06-15 21:13:34 +0530

    small changed @about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,7 @@ function About() {
                         <img src={process.env.PUBLIC_URL + "/imgs/me11.png"} className='aboutMeImg animated' alt="Message social" />
                     </div>
                     <div className="col d-flex align-self-end flex-column">
-                        <h3 className='headerText2'>hey! Aliens</h3>
+                        <h3 className='headerText2'>Hello World!</h3>
                         <p> I'm <strong> Sailendra Chettri </strong> a B Voc (SD) student studying at NBBGC Tadong.</p>
                         <p>Computers and programming have always fascinated me since when I was first introduced to them. I started learning to code when I got into college, our prof. taught us everything; from printing hello world! to building some useful stuff.</p>
                     </div>

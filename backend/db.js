56929500446f8535d516b30a69c97c503e16cc52
sailendrachettri <sailendra9083@gmail.com>
2023-07-13 15:16:22 +0530

    deployment data

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose')
-// const mongooURI = "mongodb://localhost:27017/encryasmi?readPreference=primary&appname=MongoDB%20Compass&ssl=false"
-const mongooURI = "mongodb+srv://sailendra9083:sailendra9083@encryasmi.6hiuqxk.mongodb.net/"
+const mongooURI = "mongodb+srv://sailendra9083:sailendra9083@encryasmi.6hiuqxk.mongodb.net/?retryWrites=true&w=majority"
 
 
 const connectToMongo = () => {

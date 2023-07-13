2fab482d9973e336da08315571812cde82e8d48a
sailendrachettri <sailendra9083@gmail.com>
2023-07-13 15:25:24 +0530

    deployment

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,20 @@
-const mongoose = require('mongoose')
-const mongooURI = "mongodb+srv://sailendra9083:sailendra9083@encryasmi.6hiuqxk.mongodb.net/?retryWrites=true&w=majority"
+// const mongoose = require('mongoose')
+// const mongooURI = "mongodb+srv://sailendra9083:sailendra9083@encryasmi.6hiuqxk.mongodb.net/?retryWrites=true&w=majority"
 
 
-const connectToMongo = () => {
-    mongoose.connect(mongooURI)
-}
+// const connectToMongo = () => {
+//     mongoose.connect(mongooURI)
+// }
 
-module.exports = connectToMongo
+// module.exports = connectToMongo
+
+import mongodb from 'mongodb';
+
+const MongoClient = mongodb.MongoClient;
+const uri = "mongodb+srv://sailendra9083:sailendra9083@encryasmi.6hiuqxk.mongodb.net/?retryWrites=true&w=majority"
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+    const collection = client.db("<databaseName>").collection("<collectionName>");
+    // perform actions on the collection object
+    client.close();
+});

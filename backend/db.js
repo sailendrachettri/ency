19ed2a06e1dd1cd7bdf6f93da040cd5089956ef0
sailendrachettri <sailendra9083@gmail.com>
2023-07-13 14:45:15 +0530

    deploy,ent env

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
-const mongooURI = "mongodb://localhost:27017/encryasmi?readPreference=primary&appname=MongoDB%20Compass&ssl=false"
+// const mongooURI = "mongodb://localhost:27017/encryasmi?readPreference=primary&appname=MongoDB%20Compass&ssl=false"
+const mongooURI = "mongodb+srv://sailendra9083:sailendra9083@encryasmi.6hiuqxk.mongodb.net/"
 
 
 const connectToMongo = () => {
